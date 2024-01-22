55481998b9a5ff46c1a9cec9c03e1ed8f99049b2
ParkHyunS00 <parkhyuns00@naver.com>
2024-01-22 16:58:54 +0900

    [스프링 핵심 원리 - 기본편] 스프링 핵심 원리 이해2 - 객체 지향 원리 적용 (24.01.22)

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,37 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    // 객체의 생성과 연결 담당
+
+    // MemberServiceImpl은 MemberRepository인 추상에만 의존하면 됨
+    public MemberService memberService() {
+        // MemoryMemberRepository 객체 생성하고 참조값을 생성자로 전달
+        // 클라이언트인 MemberServiceImpl 입장에서 보면 의존관계를 외부에서 주입해주는 것 같아 DI라고 함
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); // 할인 정책을 변경해도 "사용 영역"의 어떤 코드도 변경할 필요 없음
+    }
+
+    /* 역할이 전부 드러나는 코드 */
+}
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,23 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // DiscountPolicy 인터페이스만 의존함, 생성자를 통해 어떤 구현 객체가 들어올지 알 수 없음
+    // 어떤 구현 객체를 주입할 지는 오직 AppConfig 에서 결정, 실행에만 집중
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy; // 구현체에 의존하지 않도록 함(DIP는 지킴) But NullPointException
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
