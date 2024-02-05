92fb20df48207cd9c828f43d1955c7f0fbc054a7
ParkHyunS00 <parkhyuns00@naver.com>
2024-02-05 13:35:15 +0900

    [스프링 핵심 원리 - 기본편] 의존관계 자동 주입 (24.02.05)

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -12,15 +12,19 @@ import org.springframework.context.annotation.FilterType;
  * 의존관계 자동 주입하는 @Autowired 기능 제공
  */
 
+// 탐색할 패키지의 시작 위치 지정, default 는 @ComponentScan 붙은 설정 정보 클래스의 패키지가 시작 위치
+// 기존에 만들었던 Configuration 어노테이션 붙은 설정 제외 -> @Configuration 또한 자동으로 등록됨
+// 컴포넌트 스캔 사용 및 컴포넌트 스캔 대상 제외
+
 @Configuration
-@ComponentScan( // 컴포넌트 스캔 사용 및 컴포넌트 스캔 대상 제외
-        basePackages = "hello.core.member", // 탐색할 패키지의 시작 위치 지정, default 는 @ComponentScan 붙은 설정 정보 클래스의 패키지가 시작 위치
+@ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-) // 기존에 만들었던 Configuration 어노테이션 붙은 설정 제외 -> @Configuration 또한 자동으로 등록됨
+)
 public class AutoAppConfig {
 
 //    @Bean(name = "memoryMemberRepository") // 수동 등록 빈 vs 자동 등록 빈 충돌 시 수동 빈 등록이 우선권 가짐
 //    MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //    }
+
 }
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,19 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+// @RequiredArgsConstructor // 필수 값인 final 필드 초기화해주는 생성자를 만들어 줌
 public class OrderServiceImpl implements OrderService {
 
     // DiscountPolicy 인터페이스만 의존함, 생성자를 통해 어떤 구현 객체가 들어올지 알 수 없음
@@ -18,12 +22,13 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; // 구현체에 의존하지 않도록 함(DIP는 지킴) But NullPointException
 
-    // 생성자가 1개만 있다면 @Autowired 생략해도 됨
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    // 생성자가 1개만 있다면 @Autowired 생략해도 됨
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -38,3 +43,5 @@ public class OrderServiceImpl implements OrderService {
         return memberRepository;
     }
 }
+
+
