6e86181793d70445a72a011b9b0771127b948903
ParkHyunS00 <parkhyuns00@naver.com>
2024-03-18 01:12:17 +0900

    [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발] 상품 도메인 개발 (24.03.18)

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 관계 전략
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public class Item {
 
     @Id @GeneratedValue
@@ -24,4 +25,24 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // ==비즈니스 로직==// 데이터를 가지고 있는 쪽에 비즈니스 메서드가 있는 게 가장 좋다
+    // 변경할 일이 있으면 setter를 쓰는 게 아니라 핵심 비즈니스 메서드를 갖고 변경해야함
+    /**
+    *  stock 증가
+    * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *  stock 감소
+     * */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
